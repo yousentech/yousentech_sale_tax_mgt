b5cf4669c41a064a71ddb2c2ae8f0b1954b1a102
yousentech <83219678+yousentech@users.noreply.github.com>
2026-01-05 17:25:16 +0300

    updatwe

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,9 +3,20 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
  
+class xx_sale_order_line_tax_required(models.Model):
+    _inherit = 'sale.order.line'
 
-class xx_account_move_tax_required(models.Model):
-    _inherit = 'account.move'
+    l_tax_reqiured = fields.Boolean()
+
+    @api.onchange('product_id')
+    def check_tax_required(self):
+        for rec in self:
+            if rec.product_id:
+                rec.l_tax_reqiured = rec.product_id.tax_reqiured
+
+
+class xx_sale_order_tax_required(models.Model):
+    _inherit = 'sale.order'
 
     # *******************************************************************************************************
     disallow_modify_product_tax_in_invoice = fields.Boolean(
@@ -21,31 +32,18 @@ class xx_account_move_tax_required(models.Model):
     def _default_disallow_modify_product_tax_in_invoice(self):
         return self.user_has_groups('yousentech_invoicing_tax_mgt.group_disallow_modify_product_tax_in_invoice')
     # *******************************************************************************************************
-
-    @api.constrains('invoice_line_ids')
+ 
+    @api.constrains('order_line')
     def check_tax_required(self):
         for rec in self:
+            for line in rec.order_line:
+                if line.product_id:
+                    if line.product_id.tax_reqiured and not line.tax_ids:
+                        raise ValidationError(
+                                "تنبيه .. لا يمكن الاستمرار لوجود ضريبة للمنتج (%s) " % line.product_id.name)
 
-            if rec.move_type in ('out_invoice','out_refund'):
-                for line in rec.invoice_line_ids:
-                    if line.product_id:
-                        if line.product_id.tax_reqiured and not line.tax_ids:
-                            raise ValidationError(
-                                 "تنبيه .. لا يمكن الاستمرار لوجود ضريبة للمنتج (%s) " % line.product_id.name)
-
-    def post(self):
+    def action_confirm(self):
         self.check_tax_required()
-        return super(xx_account_move_tax_required, self).post()
+        return super(xx_sale_order_tax_required, self).post()
     
-class xx_account_move_line_tax_required(models.Model):
-    _inherit = 'account.move.line'
-
-    l_tax_reqiured = fields.Boolean()
-
-    @api.onchange('product_id')
-    def check_tax_required(self):
-        for rec in self:
-            if rec.product_id:
-                rec.l_tax_reqiured = rec.product_id.tax_reqiured
-
  
